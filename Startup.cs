e00fefefe70aa9e98cef6e50905c1e876d6ca579
Mohit <mohitdagar80@gmail.com>
2019-06-08 18:58:44 +0530

    New build

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace DemoApp
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("This is new dynamic build");
             });
         }
     }
